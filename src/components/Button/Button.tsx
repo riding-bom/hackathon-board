704ef703b306b301b692eb2085816766f4322406
younho <nnyyhh9247@gmail.com>
2021-04-05 13:24:39 +0900

    button wtite validation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
-import { CombinedState } from "redux";
-import { post } from "../../constance/type";
-import { addPost } from "../../firebase/API";
-import { dateAction } from "../../redux/reducers/newPost";
+import { useDispatch, useSelector } from 'react-redux';
+import { CombinedState } from 'redux';
+import { post } from '../../constance/type';
+import { addPost } from '../../firebase/API';
+import { dateAction } from '../../redux/reducers/newPost';
 
 type buttonProps = {
   className: string;
   children: React.ReactNode;
-  type?: "button" | "submit" | "reset" | undefined;
+  type?: 'button' | 'submit' | 'reset' | undefined;
   width?: string;
 };
 
@@ -20,18 +20,28 @@ const Button = ({ className, children, type }: buttonProps) => {
       }>
     ) => state.newPost
   );
+
+  // console.log(newPost);
+
+  // useSelector(store => store.newPost)
+  // newPost의 상태를 확인하여 그 상태가 비어 있는지 확인한 후 비어 있으면 클릭 이벤트가 발생하지 않도록 한다
   const dispatch = useDispatch();
 
   const onChangeIdAndDate = (e: any) => {
-    dispatch(dateAction(new Date()));
-    addPost(newPost);
+    if (newPost.title || newPost.content === '') {
+      console.log('title을 입력바랍니다.');
+      return;
+    } else {
+      dispatch(dateAction(new Date()));
+      addPost(newPost);
+    }
   };
 
   return (
     <button
       className={className}
       type={type}
-      onClick={type === "submit" ? onChangeIdAndDate : () => {}}
+      onClick={type === 'submit' ? onChangeIdAndDate : () => {}}
     >
       {children}
     </button>
@@ -39,7 +49,7 @@ const Button = ({ className, children, type }: buttonProps) => {
 };
 
 Button.defaultProps = {
-  type: "button",
+  type: 'button'
 };
 
 export default Button;
