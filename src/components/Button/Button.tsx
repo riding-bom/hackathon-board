6ddab875124e8ac7a7ecb9483561455ed2e31ff3
bg-shorthand <hanhanhan333@gmail.com>
2021-04-05 17:48:36 +0900

    go read

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,15 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { CombinedState } from 'redux';
-import { post } from '../../constance/type';
-import { addPost } from '../../firebase/API';
-import { dateAction } from '../../redux/reducers/newPost';
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { CombinedState } from "redux";
+import { post } from "../../constance/type";
+import { addPost } from "../../firebase/API";
+import { dateAction } from "../../redux/reducers/newPost";
 // import { StyledModal } from '../Modal/StyledModal';
 
 type buttonProps = {
   className: string;
   children: React.ReactNode;
-  type?: 'button' | 'submit' | 'reset' | undefined;
+  type?: "button" | "submit" | "reset" | undefined;
   width?: string;
   onOpen?: () => void;
 };
@@ -23,19 +24,23 @@ const Button = ({ className, children, type, onOpen }: buttonProps) => {
     ) => state.newPost
   );
 
+  const history = useHistory();
+  console.log(history);
+
   // console.log(newPost);
 
   // useSelector(store => store.newPost)
   // newPost의 상태를 확인하여 그 상태가 비어 있는지 확인한 후 비어 있으면 클릭 이벤트가 발생하지 않도록 한다
   const dispatch = useDispatch();
 
-  const onChangeIdAndDate = (e: any) => {
-    if (newPost.title === '' || newPost.content === '') {
+  const onChangeIdAndDate = async (e: any) => {
+    if (newPost.title === "" || newPost.content === "") {
       (onOpen as () => void)();
       return;
     } else {
       dispatch(dateAction(new Date()));
-      addPost(newPost);
+      await addPost(newPost);
+      history.push(`/${newPost.id}`);
     }
   };
 
@@ -43,7 +48,7 @@ const Button = ({ className, children, type, onOpen }: buttonProps) => {
     <button
       className={className}
       type={type}
-      onClick={type === 'submit' ? onChangeIdAndDate : () => {}}
+      onClick={type === "submit" ? onChangeIdAndDate : () => {}}
     >
       {children}
     </button>
@@ -51,7 +56,7 @@ const Button = ({ className, children, type, onOpen }: buttonProps) => {
 };
 
 Button.defaultProps = {
-  type: 'button'
+  type: "button",
 };
 
 export default Button;
